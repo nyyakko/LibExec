3b7c14e7a3bde1666dae9b4f045de63fa5c33057
nyyakko <nyakonyns@gmail.com>
2025-06-27 23:27:22 -0300

    don't read pipes when detached

--- LibExec/include/libexec/Execute.hpp
+++ LibExec/include/libexec/Execute.hpp
@@ -3,20 +3,16 @@
 #include <liberror/Result.hpp>
 
 #include <utility>
-#include <filesystem>
 #include <string>
 
 namespace libexec {
 
-enum class Mode : char
-{
+enum class Mode {
     ATTACHED,
     DETACHED
 };
 
 liberror::Result<std::pair<std::string, std::string>> execute(std::string const& command, Mode mode = Mode::ATTACHED);
-liberror::Result<void> execute(std::string const& command, std::filesystem::path const& output, Mode mode = Mode::ATTACHED);
 liberror::Result<std::pair<std::string, std::string>> execute(std::string const& command, std::vector<std::string> const& arguments, Mode mode = Mode::ATTACHED);
-liberror::Result<void> execute(std::string const& command, std::vector<std::string> const& arguments, std::filesystem::path const& output, Mode mode = Mode::ATTACHED);
 
 }
--- LibExec/source/Execute.cpp
+++ LibExec/source/Execute.cpp
@@ -7,16 +7,15 @@
 #include <sys/wait.h>
 
 #include <algorithm>
-#include <fstream>
 #include <unistd.h>
 
-liberror::Result<void> executed_command(std::array<int, 2> const& outPipe, std::array<int, 2> const& errPipe, std::string command, std::vector<std::string> arguments)
+static liberror::Result<void> executed_command(std::array<int, 2> const& stdoutPipe, std::array<int, 2> const& stderrPipe, std::string command, std::vector<std::string> arguments)
 {
-    dup2(outPipe.at(1), STDOUT_FILENO);
-    std::ranges::for_each(outPipe, close);
+    dup2(stdoutPipe.at(1), STDOUT_FILENO);
+    std::ranges::for_each(stdoutPipe, close);
 
-    dup2(errPipe.at(1), STDERR_FILENO);
-    std::ranges::for_each(errPipe, close);
+    dup2(stderrPipe.at(1), STDERR_FILENO);
+    std::ranges::for_each(stderrPipe, close);
 
     auto devNull = open("/dev/null", O_RDONLY);
     assert(devNull >= 0);
@@ -37,11 +36,11 @@ liberror::Result<void> executed_command(std::array<int, 2> const& outPipe, std::
 
 liberror::Result<std::pair<std::string, std::string>> libexec::execute(std::string const& command, std::vector<std::string> const& arguments, Mode mode)
 {
-    std::array<int, 2> outPipe {};
-    pipe(outPipe.data());
+    std::array<int, 2> stdoutPipe {};
+    pipe(stdoutPipe.data());
 
-    std::array<int, 2> errPipe {};
-    pipe(errPipe.data());
+    std::array<int, 2> stderrPipe {};
+    pipe(stderrPipe.data());
 
     auto forkId = fork();
     if (forkId == 0)
@@ -52,97 +51,72 @@ liberror::Result<std::pair<std::string, std::string>> libexec::execute(std::stri
             auto innerForkId = fork();
             if (innerForkId == 0)
             {
-                TRY(executed_command(outPipe, errPipe, command, arguments));
+                TRY(executed_command(stdoutPipe, stderrPipe, command, arguments));
             }
+
             waitpid(innerForkId, nullptr, 0);
+
             std::exit(EXIT_SUCCESS);
         }
         else
         {
-            TRY(executed_command(outPipe, errPipe, command, arguments));
+            TRY(executed_command(stdoutPipe, stderrPipe, command, arguments));
         }
     }
 
-    close(outPipe.at(1));
-    close(errPipe.at(1));
+    close(stdoutPipe.at(1));
+    close(stderrPipe.at(1));
 
-    std::string out;
-    std::string err;
+    std::string out, err;
 
-    std::array pfds {
-        pollfd { outPipe.at(0), POLLIN, 0 },
-        pollfd { errPipe.at(0), POLLIN, 0 }
-    };
-
-    for (auto reading = true; reading;)
+    if (mode == Mode::ATTACHED)
     {
-        auto result = poll(pfds.data(), pfds.size(), -1);
-        if (result <= 0) break;
+        std::array pfds {
+            pollfd { stdoutPipe.at(0), POLLIN, 0 },
+            pollfd { stderrPipe.at(0), POLLIN, 0 }
+        };
 
-        for (auto& pfd : pfds)
+        for (auto reading = true; reading;)
         {
-            if (pfd.revents & POLLIN)
-            {
-                std::array<char, 256> buffer {};
-                auto count = read(pfd.fd, buffer.data(), buffer.size());
+            auto result = poll(pfds.data(), pfds.size(), -1);
+            if (result <= 0) break;
 
-                if (pfd.fd == outPipe[0])
-                    out.append(buffer.data());
-                else
-                    err.append(buffer.data());
-
-                reading = count > 0;
-            }
-            else if (pfd.revents & (POLLHUP | POLLERR))
+            for (auto& pfd : pfds)
             {
-                reading = false;
+                if (pfd.revents & POLLIN)
+                {
+                    std::array<char, 256> buffer {};
+                    auto count = read(pfd.fd, buffer.data(), buffer.size());
+
+                    if (pfd.fd == stdoutPipe[0])
+                        out.append(buffer.data());
+                    else
+                        err.append(buffer.data());
+
+                    reading = count > 0;
+                }
+                else if (pfd.revents & (POLLHUP | POLLERR))
+                {
+                    reading = false;
+                }
             }
         }
+
+        if (out.back() == '\n') out.pop_back();
+        if (err.back() == '\n') err.pop_back();
     }
 
-    if (out.back() == '\n') out.pop_back();
-    if (err.back() == '\n') err.pop_back();
-
-    close(outPipe.at(0));
-    close(errPipe.at(0));
+    close(stdoutPipe.at(0));
+    close(stderrPipe.at(0));
 
     waitpid(forkId, nullptr, 0);
 
-    return std::make_pair(out, err);
-}
+    return std::pair { out, err };
 
-liberror::Result<void> libexec::execute(std::string const& command, std::vector<std::string> const& arguments, std::filesystem::path const& output, Mode mode)
-{
-    auto [out, err] = TRY(execute(command, arguments, mode));
-
-    if (!err.empty())
-    {
-        return liberror::make_error(err);
-    }
-
-    std::ofstream stream(output);
-    stream << out;
-
-    return {};
 }
 
 liberror::Result<std::pair<std::string, std::string>> libexec::execute(std::string const& command, Mode mode)
 {
     return execute(command, std::vector<std::string>{}, mode);
-}
-
-liberror::Result<void> libexec::execute(std::string const& command, std::filesystem::path const& output, Mode mode)
-{
-    auto [out, err] = TRY(execute(command, mode));
-
-    if (!err.empty())
-    {
-        return liberror::make_error(err);
-    }
-
-    std::ofstream stream(output);
-    stream << out;
-
-    return {};
 }
 
